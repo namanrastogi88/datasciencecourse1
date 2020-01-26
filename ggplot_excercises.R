0be3f61143c47d12197a81ac07043945f21f790e
namanrastogi88 <bitsianaman@gmail.com>
2020-01-26 18:32:50 +0530

    initial commit

--- ggplot_excercises.R
+++ ggplot_excercises.R
@@ -5,6 +5,10 @@ data("mtcars")
 mtcars$cyl_labels <- factor(as.character(mtcars$cyl), labels = c("4 Cylinder", "6 Cylinder", "8 Cylinder"))
 factor()
 
-str(mtcars)
 
-ggplot(mtcars,aes(mtcars$mpg,mtcars$disp,color=mtcars$cyl_labels)) + geom_point()+ facet_wrap(mtcars$cyl_labels) + geom_smooth()+ theme_bw()
+qplot(mtcars$mpg, mtcars$disp,data = mtcars, color = mtcars$cyl,facets = .~mtcars$cyl, geom=c("point", "smooth"))
+qplot(mtcars$mpg, data=mtcars,facets = mtcars$cyl~., binwidth = 2)
+q
+
+ggplot(mtcars,aes(mtcars$mpg,mtcars$disp,color=mtcars$cyl_labels)) + geom_density()+ facet_wrap(mtcars$cyl_labels) +  theme_bw()
+
